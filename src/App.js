c7817ebe405dd1e2a00af1eea44822c7f6f0891c
Juan Fox <foxj0875@gmail.com>
2018-04-23 15:52:44 -0400

    Cards.js component is built

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import cards from './cards.json';
 
 class App extends Component {
+
+  state = {
+    cards: cards,
+    score: 0
+  }
+
+  incrementScore () {
+    this.setState({
+      score: this.setState.score + 1
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <div>
+        <Header/>
+        <main>
+          <div className="game-board">
+            {this.state.cards.map(card => {
+              return <li 
+              className="card" 
+              key="card.number">
+                {card.number}
+              </li>
+            })}
+          </div>
+        </main>       
+      </div>  
+    )   
   }
 }
 
